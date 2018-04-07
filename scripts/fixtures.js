65270fb689bfc1b071639a9f6e2753a3680347a6
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-04-07 00:03:09 -0600

    next-prev

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+      { title: 'Blue', duration: '4:26' },
+      { title: 'Green', duration: '3:14' },
+      { title: 'Red', duration: '5:01' },
+      { title: 'Pink', duration: '3:21' },
+      { title: 'Magenta', duration: '2:15' }
+    ]
+};
+
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21' },
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15' }
+  ]
+};
+
+var albumMadMax = {
+  title: 'What a Lovely Day',
+  artist: 'Mad Max',
+  label: 'EM',
+  year: '3000',
+  albumArtUrl: 'assets/images/album_covers/07.png',
+  songs: [
+    { title: 'Get Up', duration: '1:01' },
+    { title: 'Brush Teeth', duration: '5:01' },
+    { title: 'Comb Hair', duration: '3:21' },
+    { title: 'Get Dressed', duration: '3:14' },
+    { title: 'Eat Breakfast', duration: '2:15' }
+  ]
+};
